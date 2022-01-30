0b140f1c34b046ae0659e1e9cfefdb26ede72464
SaintGemini <chacone96@gmail.com>
2022-01-30 09:32:26 -0800

    Saving changes before pull

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # crypto-data-scrapper
+
+You can download this python program and run it in the terminal to get info on the top crypto coins.
+
+To run, type the following command in the terminal:
+
+python cryptoData.py
+
--- cryptoData.py
+++ cryptoData.py
@@ -1,30 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
-from tabulate import tabulate
 
 data = []
 
 # send HTML request for page
-url = "https://finance.yahoo.com/cryptocurrencies"
+url = "https://crypto.com/price"
 results = requests.get(url)
 doc = BeautifulSoup(results.text, "html.parser")
 
 # find table of data
 coins = doc.find_all("tr")
-
 # for every row of data, get/store info
-for i in range(1, len(coins)):
+for i in range(1, 51):
     coinInfo = coins[i].find_all("td")
-    name = coinInfo[1].text
-    price = coinInfo[2].text
-    change = coinInfo[3].text
-    perChange = coinInfo[4].text
-    marketCap = coinInfo[5].text
-    vol = coinInfo[7].text
-    circSupply = coinInfo[9].text
+    name = coinInfo[2].div.a.span.text
+    price = coinInfo[3].div.text
+    change_24 = coinInfo[4].p.text
+    vol = coinInfo[5].text
+    marketCap = coinInfo[6].text
 
     # append coin info to data list
-    data.append([name, price, change, perChange, marketCap, vol, circSupply])
+    data.append([name, price, change_24, vol, marketCap])
 
-# print grid structure of data
-print(tabulate(data, headers=["Name", "Price", "Chane", "% Change", "Market Cap", "Volume in Currency (24hr)", "Circculating Supply"], tablefmt="grid"))
+print(data)
